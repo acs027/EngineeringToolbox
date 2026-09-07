6eac7f552dfc82a170cf464e4f1b63afd4e5c942
acs027 <alicihansarac@gmail.com>
2026-09-07 05:47:44 +0300

    Initial Commit

---
+++ EngineeringToolbox/EngineeringToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngineeringToolboxApp.swift
+//  EngineeringToolbox
+//
+//  Created by ali cihan on 7.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct EngineeringToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
